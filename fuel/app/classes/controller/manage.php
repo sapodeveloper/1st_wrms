aa2f0d787dceeaa9756f609c67fdecd24eae8294
morishita <tomohiro.m0219@gmail.com>
2013-06-21 12:11:04 +0900

    認証制限を実装
    管理画面にはログインしていないと見られないように記述
    未ログイン時にはログインページに強制リダイレクトされる

--- fuel/app/classes/controller/manage.php
+++ fuel/app/classes/controller/manage.php
@@ -2,6 +2,18 @@
 
 class Controller_Manage extends Controller
 {
+	public function before()
+	{
+		# もし認証チェックしてNGだったら
+		if(!Auth::check())
+		{
+			# ログインページへ移動
+			Response::redirect('index/session/login');
+		}else{
+			# 認証問題なければ何もしない
+		}
+	}
+	
 	public function action_index()
 	{
  		$view=View::forge('layout/application');
