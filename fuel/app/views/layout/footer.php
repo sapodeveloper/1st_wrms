4818bde24d8fc1fb2d22ef590840e58882affb33
morishita <tomohiro.m0219@gmail.com>
2013-06-23 16:11:10 +0900

    footerにレンダリング時間と使用メモリーを表示
    開発時に知りたいので設置

--- fuel/app/views/layout/footer.php
+++ fuel/app/views/layout/footer.php
@@ -1,7 +1,12 @@
 <hr>
 	<footer>
-  		<p align="center">
+		<p align="center">
   			&copy; ISMCサポートセンター 2013 All Rights Reserved
   		</p>
+		<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
+			<p>
+				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
+				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
+			</p>
   	</footer>
 </div>
