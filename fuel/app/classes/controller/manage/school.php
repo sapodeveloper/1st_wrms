ea63de40bb12c77e66eeaa7b320ac93a20f612cc
morishita <tomohiro.m0219@gmail.com>
2013-06-23 22:57:38 +0900

    高校情報編集機能実装

--- fuel/app/classes/controller/manage/school.php
+++ fuel/app/classes/controller/manage/school.php
@@ -67,11 +67,59 @@ class Controller_Manage_School extends Controller_Manage
 		}
 
  		$view=View::forge('layout/manage');
- 		$view->set_global('title','水ロケット管理システム(新規高校等録画面)');
+ 		$view->set_global('title','水ロケット管理システム(新規高校登録画面)');
  		$view->header=View::forge('layout/manage_header');
  		$view->side_menu=View::forge('manage/side_menu');
  		$view->content=View::forge('school/create');
  		$view->footer=View::forge('layout/footer');
  		return $view;
+	}
+
+	public function action_edit($id = null)
+	{
+		$view=View::forge('layout/manage');
+		is_null($id) and Response::redirect('index/manage/school');
+
+		if ( ! $school = Model_Create_School::find($id))
+		{
+			Session::set_flash('error', '指定されたidの高校は存在しません');
+			Response::redirect('index/manage/school');
+		}
+
+		$val = Model_Create_School::validate('edit');
+
+		if ($val->run())
+		{
+			$school->school_name = Input::post('school_name');
+			$school->school_url = Input::post('school_url');
+
+			if ($school->save())
+			{
+				Session::set_flash('success', '更新成功');
+				Response::redirect('index/manage/school');
+			}
+			else
+			{
+				Session::set_flash('error', '更新失敗');
+			}
+		}
+		else
+		{
+			if (Input::method() == 'POST')
+			{
+				$school->school_name = $val->validated('school_name');
+				$school->school_url = $val->validated('school_url');
+				Session::set_flash('error', $val->error());
+			}
+			$view->set_global('school', $school, false);
+		}
+
+ 		$view->set_global('title','水ロケット管理システム(既存高校情報編集画面)');
+ 		$view->header=View::forge('layout/manage_header');
+ 		$view->side_menu=View::forge('manage/side_menu');
+ 		$view->content=View::forge('school/edit');
+ 		$view->footer=View::forge('layout/footer');
+ 		return $view;
+
 	}
 }
