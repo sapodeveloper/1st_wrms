538d39e53534567c23d60f7e7fdf9c5434f54deb
morishita <tomohiro.m0219@gmail.com>
2013-06-23 15:17:12 +0900

    管理画面左サイドメニューのレイアウト変更
    とりあえず、管理画面トップとグループ管理トップへのリンクを生成
    必要な管理項目へのリンクはこの左サイドメニューへ追記してね

--- fuel/app/views/manage/side_menu.php
+++ fuel/app/views/manage/side_menu.php
@@ -1,3 +1,6 @@
 <div class="span2">
-    <p>グループ管理</p>
+	<ui class="nav nav-list">
+		<li><?php echo Html::anchor('/index/manage/index', '管理トップ'); ?></li>
+		<li><?php echo Html::anchor('/index/manage/group/index', 'グループ管理'); ?></li>
+	</ui>
 </div>
