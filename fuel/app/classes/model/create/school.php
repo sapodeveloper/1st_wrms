ae1c7d47c81dc6b7f8a3664a3cdb3344d0a4da9c
morishita <tomohiro.m0219@gmail.com>
2013-06-23 16:04:35 +0900

    参加校高管理モデル作成

---
+++ fuel/app/classes/model/create/school.php
@@ -0,0 +1,26 @@
+<?php
+
+class Model_Create_School extends \Orm\Model
+{
+	protected static $_properties = array(
+		'id',
+		'school_name',
+		'school_url',
+		'condition',
+		'created_at',
+		'updated_at',
+	);
+
+	protected static $_observers = array(
+		'Orm\Observer_CreatedAt' => array(
+			'events' => array('before_insert'),
+			'mysql_timestamp' => false,
+		),
+		'Orm\Observer_UpdatedAt' => array(
+			'events' => array('before_update'),
+			'mysql_timestamp' => false,
+		),
+	);
+	protected static $_table_name = 'create_schools';
+
+}
--- fuel/app/config/development/migrations.php
+++ fuel/app/config/development/migrations.php
@@ -9,6 +9,7 @@ return array(
 				0 => '001_create_users',
 				1 => '002_rename_field_group_to_group_id_in_users',
 				2 => '003_add_profile_fields_to_users',
+				3 => '004_create_create_schools',
 			),
 		),
 		'module' => 
