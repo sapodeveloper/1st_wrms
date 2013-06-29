d3f3d8067064772b113e5dbdf2341231b05d7b8a
morishita <tomohiro.m0219@gmail.com>
2013-06-29 14:58:35 +0900

    管理画面のレイアウトのリファクタリング

--- fuel/app/views/layout/manage.php
+++ fuel/app/views/layout/manage.php
@@ -24,7 +24,11 @@
 	 	<div class="navbar-inner">
 			<div class="container-fluid">
 	    		<div class="brand"><?php echo $title; ?></div>
-		    	<div class="nav-collapse collapse"></div>
+		    	<div class="nav-collapse collapse">
+		    		<p class="navbar-text pull-right">
+        				<?php echo Html::anchor('/index/session/logout', 'ログアウト'); ?>
+        			</p>
+		    	</div>
 	    	</div>
 	  	</div>
 	</div>
--- fuel/app/views/manage/side_menu.php
+++
@@ -1,7 +0,0 @@
-<div class="span2">
-	<ui class="nav nav-list">
-		<li><?php echo Html::anchor('/index/manage/index', '管理トップ'); ?></li>
-		<li><?php echo Html::anchor('/index/manage/school/index', '登録高校管理'); ?></li>
-		<li><?php echo Html::anchor('/index/manage/group/index', 'グループ管理'); ?></li>
-	</ui>
-</div>
