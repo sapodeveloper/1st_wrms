b447ba3432bf548ec55be31390e5134db52ae849
morishita <tomohiro.m0219@gmail.com>
2013-07-04 16:08:49 +0900

    空レコード一覧表示機能実装
    条件式はconditionが0のものを出すようになっているが、変更した方がいいかもしれないね。

--- fuel/app/classes/controller/manage/record.php
+++ fuel/app/classes/controller/manage/record.php
@@ -13,9 +13,10 @@ class Controller_Manage_Record extends Controller_Manage
 
 	public function action_NullAllRecord()
 	{
+		$data['records'] = Model_Record::find('all', array('where' => array('condition' => 0)));
  		$view=View::forge('layout/manage');
  		$view->set_global('title','水ロケット管理システム(記録管理画面)');
- 		$view->content=View::forge('manage/record/null_all_record');
+ 		$view->content=View::forge('manage/record/null_all_record', $data);
  		return $view;
 	}
 
--- fuel/app/views/manage/record/null_all_record.php
+++ fuel/app/views/manage/record/null_all_record.php
@@ -5,3 +5,32 @@
 	<li><?php echo Html::anchor('index/manage/record/GroupNullRecord', 'レコードなしグループ'); ?></li>
 	<li><?php echo Html::anchor('index/manage/record/SearchRecord', 'レコード検索'); ?></li>
 </ul>
+<?php if ($records): ?>
+<table class="table table-bordered">
+	<thead>
+		<td>ID</td>
+		<td>学校名</td>
+		<td>グループ名</td>
+		<td>飛距離</td>
+		<td>&nbsp;</td>
+	</thead>
+	<tbody>
+		<?php foreach ($records as $record): ?>
+			<tr>
+				<td><?php echo $record->id; ?></td>
+				<td><?php echo $record->group->school->school_name; ?></td>
+				<td><?php echo $record->group->group_name; ?></td>
+				<td><?php echo $record->x_distance; ?>m</td>
+				<td>
+					<?php echo Html::anchor('index/manage/record/view/'.$record->id, '<i class="icon-eye-open" title="View"></i>'); ?> |
+					<?php echo Html::anchor('index/manage/record/edit/'.$record->id, '<i class="icon-wrench" title="Edit"></i>'); ?> |
+					<?php echo Html::anchor('index/manage/record/delete/'.$record->id, '<i class="icon-trash" title="Delete"></i>', array('onclick' => "return confirm('削除してもいいですか？')")); ?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+<?php else: ?>
+<p>現在空レコードは記録されていません。</p>
+
+<?php endif; ?>
