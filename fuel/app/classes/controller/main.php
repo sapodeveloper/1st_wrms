c6e80e274cc8a0db67d8c24369c10e1e72b5f559
morishita <tomohiro.m0219@gmail.com>
2013-07-05 23:40:44 +0900

    トップページのレイアウト作成

--- fuel/app/classes/controller/main.php
+++ fuel/app/classes/controller/main.php
@@ -11,4 +11,26 @@ class Controller_Main extends Controller
  		$view->footer=View::forge('layout/footer');
  		return $view;
 	}
+
+	public function action_entry()
+	{
+ 		$view=View::forge('layout/application');
+ 		$view->set_global('title','水ロケット管理システム');
+ 		$view->header=View::forge('layout/header');
+ 		$view->content=View::forge('main/index');
+ 		$view->footer=View::forge('layout/footer');
+ 		return $view;
+	}
+
+	public function action_CreateGroup()
+	{
+ 		$view=View::forge('layout/application');
+ 		$view->set_global('title','水ロケット管理システム');
+ 		$view->header=View::forge('layout/header');
+ 		$view->content=View::forge('main/index');
+ 		$view->footer=View::forge('layout/footer');
+ 		return $view;
+	}
+
+
 }
---
+++ fuel/app/views/main/index.php
@@ -0,0 +1,40 @@
+<div align="center">
+	<h2>HPで登録済みの方→
+		<?php echo Html::anchor('index/main/entry', 'エントリー', array('class' => 'btn  btn-large btn-success')); ?>
+	</h2>
+	<br>
+	<h2>飛び入り参加の方→
+		<?php echo Html::anchor('index/main/CreateGroup', '飛び入り参加', array('class' => 'btn  btn-large btn-warning')); ?>
+	</h2>
+	<br>
+	<div class="row">
+    	<div class="span9">
+    		<h2>現在のトップ１０</h2>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<td>順位</td>
+						<td>学校</td>
+						<td>グループ名</td>
+						<td>スコア</td>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+    	</div>
+    	<div class="span3">
+    		<h2>現在エントリー状況</h2>
+    		<table class="table table-bordered">
+				<thead>
+					<tr>
+						<td>待ち順位</td>
+						<td>グループ名</td>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+    	</div>
+    </div>
+</div>
