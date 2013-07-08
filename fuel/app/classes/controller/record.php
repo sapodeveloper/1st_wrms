3a2932e8cffdcaaca3a9c71b2b6a4a2d17e5dec0
morishita <tomohiro.m0219@gmail.com>
2013-07-08 22:44:22 +0900

    公開記録表示機能実装

--- fuel/app/classes/controller/record.php
+++ fuel/app/classes/controller/record.php
@@ -4,10 +4,21 @@ class Controller_Record extends Controller
 {
 	public function action_index()
 	{
+		$data['distance'] = Model_Record::find('all', array('where' => array('condition' => 1), 'order_by' => array('y_distance' => 'desc'), 'limit' => 10));
+		$query = DB::query('SELECT X.id ,school_name ,group_name ,X.y_distance
+								FROM records as X
+								inner join groups on X.group_id = groups.id
+								inner join schools on groups.school_id = schools.id
+									where (X.y_distance, X.group_id)
+										in (select max(Y.y_distance), Y.group_id from records as Y group by Y.group_id)
+											order by y_distance desc');
+		$data['group_records'] = $query->as_object()->execute()->as_array();
+		$today = strtotime(date('Ymd'));
+		$data['today_records'] = Model_Record::find('all', array('where' => array(array('condition' => 1), array('created_at', '>', $today)), 'order_by' => array('y_distance' => 'desc'), 'limit' => 10));
  		$view=View::forge('layout/record');
- 		$view->set_global('title','水ロケット管理システム');
+ 		$view->set_global('title','水ロケット大会記録');
  		$view->header=View::forge('layout/header');
- 		$view->content=View::forge('record/index');
+ 		$view->content=View::forge('record/index', $data);
  		$view->footer=View::forge('layout/footer');
  		return $view;
 	}
--- fuel/app/views/record/index.php
+++ fuel/app/views/record/index.php
@@ -8,6 +8,7 @@ $(function(){
   <div class="carousel-inner">
     <div class="active item">
         <h1 align="center">飛距離ランキング</h1>
+        <h2>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -18,12 +19,23 @@ $(function(){
                 </tr>
             </thead>
             <tbody>
-                
+                <?php $count = 1; ?>
+                <?php foreach ($distance as $record): ?>
+                    <tr>
+                        <td><?php echo $count ?></td>
+                        <td><?php echo $record->group->school->school_name; ?></td>
+                        <td><?php echo $record->group->group_name; ?></td>
+                        <td><?php echo $record->y_distance; ?>m</td>
+                    </tr>
+                <?php $count++; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
+        </h2>
     </div>
     <div class="item">
         <h1 align="center">チームランキング</h1>
+        <h2>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -34,12 +46,23 @@ $(function(){
                 </tr>
             </thead>
             <tbody>
-                
+                <?php $count = 1; ?>
+                <?php foreach ($group_records as $group_record): ?>
+                    <tr>
+                        <td><?php echo $count ?></td>
+                        <td><?php echo $group_record->school_name; ?></td>
+                        <td><?php echo $group_record->group_name; ?></td>
+                        <td><?php echo $group_record->y_distance; ?>m</td>
+                    </tr>
+                <?php $count++; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
+        </h2>
     </div>
     <div class="item">
         <h1 align="center">今日のランキング</h1>
+        <h2>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -50,12 +73,19 @@ $(function(){
                 </tr>
             </thead>
             <tbody>
-                
+                <?php $count = 1; ?>
+                <?php foreach ($today_records as $today_record): ?>
+                    <tr>
+                        <td><?php echo $count ?></td>
+                        <td><?php echo $today_record->group->school->school_name; ?></td>
+                        <td><?php echo $today_record->group->group_name; ?></td>
+                        <td><?php echo $today_record->y_distance; ?>m</td>
+                    </tr>
+                <?php $count++; ?>
+                <?php endforeach; ?>        
             </tbody>
         </table>
+        </h2>
     </div>
   </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
 </div>
