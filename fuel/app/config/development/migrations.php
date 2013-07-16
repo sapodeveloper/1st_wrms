8ce8e42e443a52f8d3c418afd15a6bf00b4c1bfc
morishita <tomohiro.m0219@gmail.com>
2013-07-16 22:18:35 +0900

    migrationの変更履歴を調整

---
+++ fuel/app/config/development/migrations.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 
+			array(
+				0 => '001_create_users',
+				1 => '002_create_schools',
+				2 => '003_create_groups',
+				3 => '004_create_entries',
+				4 => '005_create_events',
+				5 => '006_create_records',
+			),
+		),
+		'module' => 
+		array(
+		),
+		'package' => 
+		array(
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
