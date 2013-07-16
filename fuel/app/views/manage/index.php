429418b9197b5543bf5ff13cefd4f14fb3345e17
morishita <tomohiro.m0219@gmail.com>
2013-07-17 00:58:52 +0900

    管理ページトップにてイベント情報が無いときの処理を実装

--- fuel/app/views/manage/index.php
+++ fuel/app/views/manage/index.php
@@ -2,4 +2,8 @@
 <h3>現在のログインアカウント</h3>
 <p><?php echo $user->get_screen_name() ?></p>
 <h3>現在開催中イベント</h3>
-<p><?php echo $event->event_name; ?></p>
+<?php if($event): ?>
+	<p><?php echo $event->event_name; ?></p>
+<?php else: ?>
+	<p>現在開催イベントが設定されていません。</p>
+<?php endif; ?>
