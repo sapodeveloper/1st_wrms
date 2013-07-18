3137e0631d558ea23c1fd29df6baab2158151a57
morishita <tomohiro.m0219@gmail.com>
2013-07-18 17:30:31 +0900

    既存グループのエントリーと飛び入り参加のためのアクションを作成

--- fuel/app/classes/controller/manage/entry.php
+++ fuel/app/classes/controller/manage/entry.php
@@ -9,4 +9,20 @@ class Controller_Manage_Entry extends Controller_Manage
  		$view->content=View::forge('manage/entry/index');
  		return $view;
 	}
+
+	public function action_regular_entry()
+	{
+		$view=View::forge('layout/manage');
+ 		$view->set_global('title','水ロケット管理システム(エントリー管理画面)');
+ 		$view->content=View::forge('manage/entry/regular_entry');
+ 		return $view;
+	}
+
+	public function action_irregular_entry()
+	{
+		$view=View::forge('layout/manage');
+ 		$view->set_global('title','水ロケット管理システム(エントリー管理画面)');
+ 		$view->content=View::forge('manage/entry/irregular_entry');
+ 		return $view;
+	}
 }
