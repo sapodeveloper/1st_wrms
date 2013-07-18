7c17213d872f997a3c06f6ed2b2972c146c3cd9d
morishita <tomohiro.m0219@gmail.com>
2013-07-19 01:41:42 +0900

    管理トップ表示情報追加

--- fuel/app/classes/controller/manage.php
+++ fuel/app/classes/controller/manage.php
@@ -16,8 +16,66 @@ class Controller_Manage extends Controller
 	
 	public function action_index()
 	{
-		$data['event'] = Model_Event::find('first', array('where' => array('condition' => 1)));
-		$data['record'] = Model_Record::find('all', array('where' => array('condition' => 1)));
+		$data['event']         = Model_Event::find('first', array('where' => array('condition' => 1)));
+		$event_data            = Model_Event::find('first', array('where' => array('condition' => 1)));
+		if($event_data)
+		{
+			$event_id              = $event_data->id;
+		}
+		else
+		{
+			$event_id              = 0;
+		}
+		$data['record']        = Model_Record::find('all', array('where' => array('condition' => 1)));
+		# 状況
+		$data['create_rokect'] = count(DB::select()->from('entries')->where('condition', '=', 1)->execute());
+		$data['standby']       = count(DB::select()->from('entries')->where('condition', '=', 2)->execute());
+		$data['recording']     = count(DB::select()->from('entries')->where('condition', '=', 3)->execute());
+		$data['repair_rokect'] = count(DB::select()->from('entries')->where('condition', '=', 7)->execute());
+		# 記録状況
+		$query = DB::select()->from('records');
+		$data['all_records'] = count($query->execute());
+		$all_distances = Model_Record::find('all');
+		$sum = 0;
+		foreach ($all_distances as $all_distance)
+		{
+			$sum += $all_distance->y_distance;
+		}
+		$data['ave_distance'] = $sum / $data['all_records'];
+		$max_records = Model_Record::find('first', array('order_by' => array('y_distance' => 'desc')));
+		if($max_records)
+		{
+			$data['max_distance'] = $max_records->y_distance;
+		}
+		else
+		{
+			$data['max_distance'] = 0;
+		}
+
+		$data['event_records'] = count($query->where('event_id', '=', $event_id)->execute());
+		$event_all_distances = Model_Record::find('all', array('where' => array('event_id' => $event_id)));
+		$sum = 0;
+		foreach ($event_all_distances as $event_all_distance)
+		{
+			$sum += $event_all_distance->y_distance;
+		}
+		if($sum == 0)
+		{
+			$data['event_ave_distance'] = 0;
+		}
+		else
+		{
+			$data['event_ave_distance'] = $sum / $data['event_records'];
+		}
+		$max_records = Model_Record::find('first', array('where' => array('event_id' => $event_id), 'order_by' => array('y_distance' => 'desc')));
+		if($max_records)
+		{
+			$data['event_max_distance'] = $max_records->y_distance;
+		}
+		else
+		{
+			$data['event_max_distance'] = 0;
+		}
  		$view=View::forge('layout/manage');
  		$view->set_global('title','水ロケット管理システム(管理画面)');
  		$view->content=View::forge('manage/index', $data);
--- fuel/app/views/manage/index.php
+++ fuel/app/views/manage/index.php
@@ -1,9 +1,69 @@
 <?php $user = Auth::instance(); ?>
-<h3>現在のログインアカウント</h3>
-<p><?php echo $user->get_screen_name() ?></p>
-<h3>現在開催中イベント</h3>
-<?php if($event): ?>
-	<p><?php echo $event->event_name; ?></p>
-<?php else: ?>
-	<p>現在開催イベントが設定されていません。</p>
-<?php endif; ?>
+<div class="row">
+	<div class="span3">
+		<h3>ログインアカウント</h3>
+		<p><?php echo $user->get_screen_name() ?></p>
+		<h3>現在開催中イベント</h3>
+		<?php if($event): ?>
+			<p><?php echo $event->event_name; ?></p>
+		<?php else: ?>
+			<p>現在開催イベントが設定されていません。</p>
+		<?php endif; ?>
+	</div>
+	<div class="span4">
+		<table class="table table-bordered">
+			<tr>
+				<td colspan="2">状況</td>
+			</tr>
+			<tr>
+				<td>ロケット作成中</td>
+				<td><?php echo $create_rokect; ?></td>
+			</tr>
+			<tr>
+				<td>スタンバイ</td>
+				<td><?php echo $standby; ?></td>
+			</tr>
+			<tr>
+				<td>記録中</td>
+				<td><?php echo $recording; ?></td>
+			</tr>
+			<tr>
+				<td>ロケット修理中</td>
+				<td><?php echo $repair_rokect; ?></td>
+			</tr>
+		</table>
+	</div>
+	<div class="span5">
+		<table class="table table-bordered">
+			<tr>
+				<td colspan="3">記録状況</td>
+			</tr>
+			<tr>
+				<td rowspan="3">全記録</td>
+				<td>全記録数</td>
+				<td><?php echo $all_records; ?>件</td>
+			</tr>
+			<tr>
+				<td>全記録平均飛距離</td>
+				<td><?php echo sprintf("%.2f", $ave_distance); ?>m</td>
+			</tr>
+			<tr>
+				<td>全記録最高飛距離</td>
+				<td><?php echo $max_distance; ?>m</td>
+			</tr>
+			<tr>
+				<td rowspan="3">開催中イベント</td>
+				<td>記録数</td>
+				<td><?php echo $event_records; ?>件</td>
+			</tr>
+			<tr>
+				<td>平均飛距離</td>
+				<td><?php echo sprintf("%.2f", $event_ave_distance); ?>m</td>
+			</tr>
+			<tr>
+				<td>最高飛距離</td>
+				<td><?php echo $event_max_distance; ?>m</td>
+			</tr>
+		</table>
+	</div>
+</div>
